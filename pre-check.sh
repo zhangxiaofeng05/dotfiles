bc7c7079ae23a5757767a3b29aef9ecbd9e4334d
zhangxiaofeng05 <zhangxiaofeng05@foxmail.com>
2023-02-22 17:47:26 +0800

    feat(check): update

--- pre-check.sh
+++ pre-check.sh
@@ -11,5 +11,23 @@ tree --version
 # https://github.com/prettier/prettier
 # npm -g install prettier
 prettier --version
+
+# others
 # https://github.com/jesseduffield/lazygit
 lazygit --version
+# https://github.com/tldr-pages/tldr
+tldr --version
+# https://github.com/gohugoio/hugo#choose-how-to-install
+hugo version
+# https://github.com/soimort/you-get#installation
+you-get --version
+
+# https://github.com/commitizen/cz-cli#conventional-commit-messages-as-a-global-utility
+if type commitizen > /dev/null 2>&1; then
+	echo 'commitizen command exist'
+else
+	echo 'commitizen command not exist'
+	exit 1
+fi
+
+echo -e '\nfinish check!!!'
